15a5be89ab481e6c4eedea2e43a41dc46dc20418
Cristian Prochnow <cristianprochnow@gmail.com>
2021-02-22 11:11:58 -0300

    feat(components): create experience bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css'
 
 function App() {
-  return <h1>Hello NLW delicinha!</h1>;
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
 }
 
 export default App;
