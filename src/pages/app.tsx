b05c72f80d2b085c2d8e260cf4bd157ab04e10dc
Cristian Prochnow <cristianprochnow@gmail.com>
2021-03-06 18:58:14 -0300

    feat: create styles for homepage

---
+++ src/pages/app.tsx
@@ -0,0 +1,67 @@
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import styles from '../styles/pages/App.module.css'
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
+interface HomeProps {
+  level: number
+  currentExperience: number
+  completedChallenges: number
+}
+
+export default function App({
+  level,
+  currentExperience,
+  completedChallenges
+}: HomeProps) {
+  return (
+    <ChallengesProvider
+      level={level}
+      currentExperience={currentExperience}
+      completedChallenges={completedChallenges}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>App | Move.it</title>
+        </Head>
+
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const {
+    level,
+    currentExperience,
+    completedChallenges
+  } = context.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      completedChallenges: Number(completedChallenges),
+    }
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,28 @@
-import { GetServerSideProps } from 'next'
-import Head from 'next/head'
 import styles from '../styles/pages/Home.module.css'
-import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from '../components/Profile';
-import { CompletedChallenges } from '../components/CompletedChallenges';
-import { Countdown } from '../components/Countdown';
-import { ChallengeBox } from '../components/ChallengeBox';
-import { CountdownProvider } from '../contexts/CountdownContext';
-import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-interface HomeProps {
-  level: number
-  currentExperience: number
-  completedChallenges: number
-}
-
-export default function Home({
-  level,
-  currentExperience,
-  completedChallenges
-}: HomeProps) {
+export default function Home() {
   return (
-    <ChallengesProvider
-      level={level}
-      currentExperience={currentExperience}
-      completedChallenges={completedChallenges}
-    >
-      <div className={styles.container}>
-        <Head>
-          <title>Início | Move.it</title>
-        </Head>
-
-        <ExperienceBar />
-
-        <CountdownProvider>
-          <section>
-            <div>
-              <Profile />
-              <CompletedChallenges />
-              <Countdown />
-            </div>
-            <div>
-              <ChallengeBox />
-            </div>
-          </section>
-        </CountdownProvider>
+    <div className={styles.homeContainer}>
+      <div id={styles.heroImage}>
+        <img src="./hero-image.svg" alt="Hero image"/>
       </div>
-    </ChallengesProvider>
+      <section id={styles.homePresentation}>
+        <img src="./logo-full-white.svg" alt="Move.it"/>
+
+        <h1>Bem-vindo</h1>
+
+        <span>
+          <img src="./icons/github.svg" alt="Github"/>
+          <strong>Faça login com seu Github para começar</strong>
+        </span>
+
+        <div>
+          <input type="text" placeholder="Digite seu username" />
+          <button type="button">
+            <img src="./icons/arrow-right.svg" alt="Go to next page"/>
+          </button>
+        </div>
+      </section>
+    </div>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const {
-    level,
-    currentExperience,
-    completedChallenges
-  } = context.req.cookies
-
-  return {
-    props: {
-      level: Number(level),
-      currentExperience: Number(currentExperience),
-      completedChallenges: Number(completedChallenges),
-    }
-  }
 }
