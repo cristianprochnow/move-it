9b416ab77b060c6185506bcbcb6b9a0ba9057efb
Cristian Prochnow <cristianprochnow@gmail.com>
2021-03-19 20:23:49 -0300

    feat: update signup method with addictional informations to the database

--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import axios from 'axios'
 import { Db } from 'mongodb'
 import { connectToDatabase } from '../../utils/connectToDatabase'
-
 
 const { MONGODB_URI } = process.env
 let cachedDatabaseConnection: Db = null
@@ -9,17 +9,41 @@ let cachedDatabaseConnection: Db = null
 export default async (request: VercelRequest, response: VercelResponse) => {
   const { githubUsername } = request.body
 
-  cachedDatabaseConnection = await connectToDatabase(process.env.MONGODB_URI, cachedDatabaseConnection)
+  try {
+    await axios.get(`https://api.github.com/users/${githubUsername}`)
+  } catch (error) {
+    return response
+      .status(404)
+      .json({
+        error: `${githubUsername} was not a valid GitHub username`,
+        details: error
+      })
+  }
+
+  cachedDatabaseConnection = await connectToDatabase(MONGODB_URI, cachedDatabaseConnection)
   const collection = cachedDatabaseConnection.collection('app')
 
   try {
     const registerResponse = await collection.insertOne({
       githubUsername,
+      level: 0,
+      completedChallenges: 0,
+      currentExperience: 0,
       joinedAt: new Date()
     })
 
-    return response.status(201).json({ signUpId: registerResponse.insertedId })
+    return response
+      .status(201)
+      .json({
+        signUpId: registerResponse.insertedId,
+        githubUsername
+      })
   } catch (error) {
-    return response.status(500).json({ error })
+    return response
+      .status(500)
+      .json({
+        error: 'An error ocurred at database registering',
+        details: error
+      })
   }
 }
