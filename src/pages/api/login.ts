09943c3b018a2f60e3cb9f6f6c47e4e9d6f1845f
Cristian Prochnow <cristianprochnow@gmail.com>
2021-03-19 20:32:59 -0300

    feat: create login method

---
+++ src/pages/api/login.ts
@@ -0,0 +1,32 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Db } from "mongodb";
+import { connectToDatabase } from "../../utils/connectToDatabase";
+
+const { MONGODB_URI } = process.env
+let cachedDatabaseConnection: Db = null
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const { githubUsername } = request.body
+
+  cachedDatabaseConnection = await connectToDatabase(
+    MONGODB_URI,
+    cachedDatabaseConnection
+  )
+
+  const collection = cachedDatabaseConnection.collection('app')
+
+  try {
+    const registeredUser = await collection.findOne({ githubUsername })
+
+    return response
+      .status(200)
+      .json(registeredUser)
+  } catch (error) {
+    return response
+      .status(404)
+      .json({
+        error: 'User was not found',
+        details: error
+      })
+  }
+}
